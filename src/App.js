b36b2bdad296f02b999a12454dfa4590a23787d8
Rana Bhattacharyya <ranab33@gmail.com>
2019-09-26 22:34:49 -0400

    loading data with axios and displaying

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import lDataLoader from "./Dataloader";
 import './App.css';
-import Timer from './Timer';
+import ZenQuote from './ZenQuote';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <h1>TIMER</h1>
-        <Timer />
+        <ZenQuote />
       </div>
     );
   }
